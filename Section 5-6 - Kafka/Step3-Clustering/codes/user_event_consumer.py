e23189607efa8ef573c95dfa6158d8f772ff2403
Mojtaba Banaie <mojtaba.banaie@gmail.com>
2021-12-05 14:03:04 +0330

    kafka

---
+++ Section 5-6 - Kafka/Step3-Clustering/codes/user_event_consumer.py
@@ -0,0 +1,22 @@
+import json
+from kafka import KafkaConsumer
+
+TOPIC_NAME = 'events_topic'
+
+consumer = KafkaConsumer(
+    TOPIC_NAME,
+    auto_offset_reset='earliest', # where to start reading the messages at
+    group_id='event-collector-group-1', # consumer group id
+    bootstrap_servers=['kafka1:9091','kafka2:9092','kafka3:9093'],
+    value_deserializer=lambda m: json.loads(m.decode('utf-8')) ,# we deserialize our data from json
+    security_protocol= 'PLAINTEXT'
+)
+
+def consume_events():
+    for message in consumer:
+        # any custom logic you need
+        print(f"Partition:{message.partition}\tOffset:{message.offset}\tKey:{message.key}\tValue:{message.value}")
+
+if __name__ == '__main__':
+    print("Consumer Started ...")
+    consume_events()
